1084d0d432f6ffcc6c22500f3bce05228c1ec384
Evgeniy Glukhov <evgeniy.glukhov@jetbrains.com>
2024-04-02 09:41:26 +0200

    Add filtering to repository snapshots Implement filtering strategies from the Stack 2

--- data_classes/datapoint_py.py
+++ data_classes/datapoint_py.py
@@ -1,13 +1,14 @@
 from dataclasses import asdict
 
 from data_classes.datapoint_base import DatapointBase
+from data_filters.repo_snapshot_filter_base import SnapshotFilterBase
 
 
 class DatapointPy(DatapointBase):
     @classmethod
-    def from_hf_datapoint(cls, hf_dp: dict):
+    def from_hf_datapoint(cls, hf_dp: dict, **kwargs):
         completion_dict = {hf_dp['completion_file']['filename']: hf_dp['completion_file']['content']}
-        context_dict = cls._preprocess_repo_snapshot(hf_dp['repo_snapshot'])
+        context_dict = cls._preprocess_repo_snapshot(hf_dp['repo_snapshot'], **kwargs)
         dp_args = {
             'repo': hf_dp['repo'],
             'commit': hf_dp['commit_hash'],
@@ -19,7 +20,8 @@ class DatapointPy(DatapointBase):
         return cls(**dp_args)
 
     @staticmethod
-    def _preprocess_repo_snapshot(repo_snapshot_raw: dict[str: list[str]]) -> dict[str, str]:
+    def _preprocess_repo_snapshot(repo_snapshot_raw: dict[str: list[str]],
+                                  filtering: SnapshotFilterBase | None = None) -> dict[str, str]:
         repo_snapshot_dict = {filename: content for filename, content in
                               zip(repo_snapshot_raw['filename'], repo_snapshot_raw['content'])}
         empty_filenames = [filename for filename, content in repo_snapshot_dict.items() if content is None or
@@ -27,7 +29,8 @@ class DatapointPy(DatapointBase):
         for filename in empty_filenames:
             repo_snapshot_dict.pop(filename)
 
-        # TODO: Implement content filters from The Stack 2 Dataset
+        if filtering is not None:
+            repo_snapshot_dict = filtering.filter_repo(repo_snapshot_dict)
 
         return repo_snapshot_dict
 
