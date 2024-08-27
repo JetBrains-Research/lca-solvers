8917e63774d6ebf25e53a4e8169419ba1746eb4a
tejpaper <dev.assistant.re@gmail.com>
2024-08-27 14:26:27 +0000

    New runs

---
+++ runs/RelativeCalls_FullFT_DeepSeekCoder1p3Base_HP001/run_script.sh
@@ -0,0 +1,4 @@
+/home/sapronov/.virtualenvs/lca-solvers/bin/python3 \
+/home/sapronov/lca-solvers/pipeline/__main__.py \
+run_name=RelativeCalls_FullFT_DeepSeekCoder1p3Base_HP001 \
+composer=chained_composer/most_relative_callable
