09fc3e232bb722e578f8e728a9b965958e0adaa6
tejpaper <dev.assistant.re@gmail.com>
2024-09-02 16:16:03 +0000

    A batch of new runs

---
+++ runs/LowTokensRatioFiltering_FullFT_DeepSeekCoder1p3Base_HP001/run_script.sh
@@ -0,0 +1,5 @@
+/home/sapronov/.virtualenvs/lca-solvers/bin/python3 \
+/home/sapronov/lca-solvers/pipeline/__main__.py \
+run_name=LowTokensRatioFiltering_FullFT_DeepSeekCoder1p3Base_HP001 \
+composer=chained_composer/low_tokens_ratio_filtering \
+trainer=full_finetuning_trainer/more_workers
---
+++ runs/MediumLines_FullFT_DeepSeekCoder1p3Base_HP001/run_script.sh
@@ -0,0 +1,5 @@
+/home/sapronov/.virtualenvs/lca-solvers/bin/python3 \
+/home/sapronov/lca-solvers/pipeline/__main__.py \
+run_name=MediumLines_FullFT_DeepSeekCoder1p3Base_HP001 \
+composer=chained_composer/medium_lines \
+trainer=full_finetuning_trainer/more_workers
