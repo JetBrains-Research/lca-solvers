e8be71c09b5c8d34a04ebbf691b4b7c80455043e
tejpaper <dev.assistant.re@gmail.com>
2024-09-13 14:23:00 +0000

    Baseline pipeline

--- cache_study/model.py
+++ cache_study/model.py
@@ -1,6 +1,66 @@
-import copy
+from cache_study.utils import get_free_device, get_optimal_dtype
 
+import copy
+from enum import Enum
+
+import torch
 import torch.nn as nn
+from transformers.models.auto import MODEL_FOR_CAUSAL_LM_MAPPING
+from transformers.utils import is_flash_attn_2_available, is_torch_sdpa_available
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    AutoConfig,
+    PreTrainedModel,
+    PreTrainedTokenizerBase,
+)
+
+
+class AttentionImplementation(str, Enum):
+    FA2 = 'flash_attention_2'
+    SDPA = 'sdpa'
+    EAGER = 'eager'
+
+
+def get_optimal_attn(model_name: str, device: torch.device, dtype: torch.dtype) -> AttentionImplementation:
+    hf_model_config = AutoConfig.from_pretrained(model_name)
+    model_cls = MODEL_FOR_CAUSAL_LM_MAPPING[type(hf_model_config)]
+
+    fa2_supported = (
+            is_flash_attn_2_available() and
+            model_cls._supports_flash_attn_2 and  # noqa: HF doesn't have an API for this case
+            device.type == 'cuda' and
+            dtype in (torch.float16, torch.bfloat16)
+    )
+
+    if fa2_supported:
+        return AttentionImplementation.FA2
+    elif is_torch_sdpa_available() and model_cls._supports_sdpa:  # noqa: same
+        return AttentionImplementation.SDPA
+    else:
+        return AttentionImplementation.EAGER
+
+
+def init_tokenizer_model(model_name: str) -> tuple[PreTrainedTokenizerBase, PreTrainedModel]:
+    device = get_free_device()
+    dtype = get_optimal_dtype()
+    attn_implementation = get_optimal_attn(model_name, device, dtype)
+    torch.set_float32_matmul_precision('high')
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_name,
+        trust_remote_code=True)
+    tokenizer.padding_side = 'right'
+
+    model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=model_name,
+        trust_remote_code=True,
+        device_map=device,
+        torch_dtype=dtype,
+        attn_implementation=attn_implementation,
+    ).eval().requires_grad_(False)
+
+    return tokenizer, model
 
 
 def create_model_reference(model: nn.Module) -> nn.Module:
@@ -10,12 +70,12 @@ def create_model_reference(model: nn.Module) -> nn.Module:
     return model_ref
 
 
-def split_model(model: nn.Module, num_gen_blocks: int) -> tuple[nn.Module, nn.Module]:
-    indexer = create_model_reference(model.model)
-    indexer.layers = indexer.layers[:-num_gen_blocks]
-    indexer.norm = nn.Identity()
+def split_model(model: nn.Module, num_gen_layers: int) -> tuple[nn.Module, nn.Module]:
+    encoder = create_model_reference(model.model)
+    encoder.layers = encoder.layers[:-num_gen_layers]
+    encoder.norm = nn.Identity()
 
     generator = create_model_reference(model)
-    generator.model.layers = generator.model.layers[-num_gen_blocks:]
+    generator.model.layers = generator.model.layers[-num_gen_layers:]
 
-    return indexer, generator
+    return encoder, generator
--- cache_study/pipeline.py
+++ cache_study/pipeline.py
@@ -1,17 +1,29 @@
-from cache_study.metric import Metrics
 from cache_study.encoder import CacheEncoderMixin
 from cache_study.generator import GeneratorMixin
-from cache_study.preprocessor import PreprocessorMixin
+from cache_study.preprocessor import BOSUsage, PreprocessorMixin
+from composers.chain.chain import UnsafeComposerChain
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 from typing import Any
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import PreTrainedTokenizerBase
+
+
+@dataclass
+class Metrics:
+    cross_entropy: list[float] = field(default_factory=list)
+    exact_match: list[float] = field(default_factory=list)
 
 
 class Pipeline(ABC):
-    def eval(self, logits: list[torch.Tensor], target_ids: torch.Tensor) -> Metrics:
-        pass  # TODO
+    @torch.inference_mode
+    def eval(self, logits: torch.Tensor, target_ids: torch.Tensor) -> tuple[float, float]:
+        loss = F.cross_entropy(logits, target_ids).item()
+        return loss, -1  # TODO: add Exact Match
 
     @abstractmethod
     def __call__(self, datapoint: dict[str, Any]) -> Metrics:
@@ -19,7 +31,28 @@ class Pipeline(ABC):
 
 
 class BaselinePipeline(Pipeline, PreprocessorMixin, GeneratorMixin):
-    pass  # TODO
+    def __init__(self,
+                 composer: UnsafeComposerChain,
+                 tokenizer: PreTrainedTokenizerBase,
+                 bos_usage: BOSUsage,
+                 full_model: nn.Module,
+                 ) -> None:
+        if bos_usage == BOSUsage.TAIL_SEAM:
+            raise ValueError('Tail seaming is only possible for TrialPipeline.')
+
+        PreprocessorMixin.__init__(self, composer, tokenizer, bos_usage)
+        GeneratorMixin.__init__(self, full_model, hs_input=False)
+
+    def __call__(self, datapoint: dict[str, Any]) -> Metrics:
+        head_blocks, tail_blocks, target_ids = self.preprocess(datapoint, self.model.device)
+        metrics = Metrics()
+
+        for logits in self.produce_logits(head_blocks, tail_blocks, len(target_ids)):
+            loss, em = self.eval(logits, target_ids)
+            metrics.cross_entropy.append(loss)
+            metrics.exact_match.append(em)
+
+        return metrics
 
 
 class TrialPipeline(Pipeline, PreprocessorMixin, CacheEncoderMixin, GeneratorMixin):
