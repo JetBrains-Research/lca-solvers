8ac613cec5f5512df41c3e69d35823ae7122c10c
tejpaper <dev.assistant.re@gmail.com>
2024-09-25 22:23:32 +0000

    New runs

---
+++ runs/PythonFilesWithSeparator_FullFT_DeepSeekCoder1p3Base_HP001/run_script.sh
@@ -0,0 +1,5 @@
+/home/sapronov/.virtualenvs/lca-solvers/bin/python3 \
+/home/sapronov/lca-solvers/pipeline/__main__.py \
+run_name=PythonFilesWithSeparator_FullFT_DeepSeekCoder1p3Base_HP001 \
+composer=chained_composer/python_files \
+preprocessor=completion_loss_preprocessor/full_completion_loss_with_sep_16k
---
+++ runs/SmoothPrefixUnmaskWithSeparator_FullFT_DeepSeekCoder1p3Base_HP001/run_script.sh
@@ -0,0 +1,7 @@
+/home/sapronov/.virtualenvs/lca-solvers/bin/python3 \
+/home/sapronov/lca-solvers/pipeline/__main__.py \
+run_name=SmoothPrefixUnmaskWithSeparator_FullFT_DeepSeekCoder1p3Base_HP001 \
+adapter=smooth_prefix_unmask_adapter/all_params \
+composer=chained_composer/python_files \
+preprocessor=completion_loss_preprocessor/full_completion_loss_with_sep_16k \
+trainer=full_finetuning_trainer/past_weights
